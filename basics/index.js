301f1a1a26a091361f5ef086a4cab5d744cb6131
Essien Emmanuel <essienemma300@gmail.com>
2026-02-22 21:53:23 +0100

    #1 circle learn

--- basics/index.js
+++ basics/index.js
@@ -6,9 +6,24 @@ const ctx = canvas.getContext("2d");
 document.addEventListener("resize", () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-  ctx.fillStyle = "white";
-  ctx.fillRect(10, 20, 150, 50);
+  drawCircle(mouse.x, mouse.y);
 });
 
-ctx.fillStyle = "white";
-ctx.fillRect(10, 20, 150, 50);
+const mouse = {
+  x: null,
+  y: null,
+};
+document.addEventListener("click", (e) => {
+  mouse["x"] = e.x;
+  mouse["y"] = e.y;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawCircle();
+});
+
+function drawCircle() {
+  ctx.strokeStyle = "red";
+  ctx.lineWidth = 5;
+  ctx.beginPath();
+  ctx.arc(mouse.x, mouse.y, 50, 0, 2 * Math.PI);
+  ctx.stroke();
+}
